801f6bd1c49128017bac1bfa75e7dc511ac61ba7
Abdussalam <abdussalam999948@gmail.com>
2018-02-19 05:25:25 +0200

    go

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 */
-Route::get('/', 'PagesController@index');
+Route::get('/', 'PostsController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 

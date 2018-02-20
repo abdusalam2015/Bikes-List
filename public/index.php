89352ebe62f6953418c9da23dbdffa05d8b13c50
Abdussalam <abdussalam999948@gmail.com>
2018-02-20 11:57:31 +0200

    final in a right way6

--- public/index.php
+++ public/index.php
@@ -22,7 +22,6 @@ define('LARAVEL_START', microtime(true));
 */
 
 require __DIR__.'/../vendor/autoload.php';
-
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +34,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../atco/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------

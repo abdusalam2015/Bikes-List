e237f5a54795780a1d323d72b62bee3506f115ce
Abdussalam <abdussalam999948@gmail.com>
2018-02-20 12:16:52 +0200

    final in a right way12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'ATCO') }}</title>
+    <title>ATCO</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">

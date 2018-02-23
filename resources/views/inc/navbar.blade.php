8273812be10c407b94e2eabd0af157868945a795
Abdussalam <abdussalam999948@gmail.com>
2018-02-23 04:01:51 +0200

    Last Update11

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,3 +1,5 @@
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">

4d408cf11fb5f6aa9c4be4369519854d9e22c7c5
Abdussalam <abdussalam999948@gmail.com>
2018-02-24 16:47:50 +0200

    make it better1

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../arcane-brook-40749/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -34,7 +34,7 @@ require __DIR__.'/../arcane-brook-40749/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../arcane-brook-40749/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
